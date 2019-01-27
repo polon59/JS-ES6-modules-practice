4a59cbd8970932f9cbd2e41e643e7df299c8b29d
polon59 <przemyslaw.polczak@gmail.com>
2019-01-27 21:13:51 +0100

    Remove application.js import statement

--- JS/Controller/Controller.js
+++ JS/Controller/Controller.js
@@ -1,16 +1,5 @@
-import {Application} from "../Application.js";
-
 export class Controller{
 
-    constructor(){
-        this.app = new Application();
-        this.button = $("#calculteButton");
-        this.initializeEventListeners();
-    }
-
-    initializeEventListeners(){
-        this.button.click(this.app.handleApp());
-    }
 
     provideDataForCalculation(){
         // change to get input from form
